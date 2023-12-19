68aa3f8d1f2034b4da8af70aff1b99b949bcf421
JMcK4529 <07josephmckeown@gmail.com>
2023-12-19 16:28:44 +0000

    Add try/except block to pass PSQL env variables during build test

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -23,9 +23,24 @@ class DatabaseConnection:
             self.connection = psycopg.connect(
                 f"postgresql://localhost/{self._database_name()}",
                 row_factory=dict_row)
-        except psycopg.OperationalError:
-            raise Exception(f"Couldn't connect to the database {self._database_name()}! " \
-                    f"Did you create it using `createdb {self._database_name()}`?")
+        except:
+            try:
+                db_host = "localhost"
+                db_name = self._database_name()
+                db_user = os.getenv("POSTGRES_USER")
+                db_password = os.getenv("POSTGRES_PASSWORD")
+
+                # Construct the connection string
+                connection_string = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
+
+                self.connection = psycopg.connect(
+                    connection_string,
+                    row_factory=dict_row)
+            except psycopg.OperationalError:
+                raise Exception(f"Couldn't connect to the database {self._database_name()}! " \
+                        f"Did you create it using `createdb {self._database_name()}`?")
+            
+
 
     # This method seeds the database with the given SQL file.
     # We use it to set up our database ready for our tests or application.
