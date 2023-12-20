0e07fb5b05b2519c0ad55305f27069c991f94606
JMcK4529 <07josephmckeown@gmail.com>
2023-12-20 21:21:43 +0000

    Correct CREATE DATABASE commands in psql_setup.py

--- psql_setup.py
+++ psql_setup.py
@@ -2,5 +2,5 @@ import psycopg
 
 conn = psycopg.connect(user="postgres", host="localhost", password="postgres", autocommit=True)
 cursor = conn.cursor()
-cursor.execute("createdb chitter;")
-cursor.execute("createdb chitter_test;")
+cursor.execute("CREATE DATABASE chitter;")
+cursor.execute("CREATE DATABASE chitter_test;")
