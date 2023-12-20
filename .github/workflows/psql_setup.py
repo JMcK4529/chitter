d8183f1421a776c58a08fca7448cccc83a710bd4
JMcK4529 <07josephmckeown@gmail.com>
2023-12-20 16:41:24 +0000

    Debug attempt yml uses .github default

--- .github/workflows/psql_setup.py
+++
@@ -1,6 +0,0 @@
-import psycopg
-
-conn = psycopg.connect(user="postgres", password="postgres", autocommit=True)
-cursor = conn.cursor()
-cursor.execute("createdb chitter;")
-cursor.execute("createdb chitter_test;")
