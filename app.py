d02ca5725b508a7b5fae592d3d1549a960fe6a9f
JMcK4529 <07josephmckeown@gmail.com>
2023-12-24 13:31:44 +0000

    Add sessions with login and logout

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
-from flask import Flask, render_template
+from dotenv import load_dotenv
+from flask import Flask, render_template, url_for, session, request, redirect
 from lib.user_repository import UserRepository
 from lib.peep_repository import PeepRepository
 from lib.database_connection import get_flask_database_connection
+load_dotenv(override=True)
 
 app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = os.getenv('SESSION_KEY')
 
 @app.route('/', methods=['GET'])
 def get_index():
@@ -13,8 +16,70 @@ def get_index():
     peep_repo = PeepRepository(connection)
     users = user_repo.all()
     peeps = peep_repo.all()
-    return render_template('index.html', users=users, peeps=peeps)
+    if 'user_id' in session:
+        return render_template(
+            'index.html', 
+            current_user=user_repo.find(session['user_id']).username, 
+            users=users, 
+            peeps=peeps
+            )
+    else:
+        return render_template(
+            'index_with_login_prompt.html', 
+            users=users, 
+            peeps=peeps
+            )
+
+@app.route('/login', methods=['GET'])
+def get_login():
+    return render_template('login.html', error=None)
+
+@app.route('/login', methods=['POST'])
+def post_login():
+    connection = get_flask_database_connection(app)
+    username = request.form['Username or Email']
+    password = request.form['Password']
+    user_repo = UserRepository(connection)
+    try:
+        if user_repo.check_password(username, password):
+            try:
+                user = user_repo.find_by_email(username)
+                session['user_id'] = user.id
+                return redirect(url_for('get_index'))
+            except:
+                try:
+                    user = user_repo.find_by_username(username)
+                    session['user_id'] = user.id
+                    return redirect(url_for('get_index'))
+                except:
+                    return render_template(
+                        'login.html', 
+                        error="Something went wrong with your login.")
+        else:
+            raise Exception()
+    except:
+        return render_template(
+            'login.html', 
+            error="Username or password was incorrect.")
+    
+@app.route("/logout", methods=['POST'])
+def post_logout():
+    if session['user_id']:
+        del session['user_id']
+        if 'user_id' not in session:
+            return redirect(url_for('get_index'))
+    else:
+        return None
+    
+@app.route("/signup", methods=['GET'])
+def get_signup():
+    return None
+        
 
 if __name__ == "__main__":
     #app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
+    app.run(
+        debug=True, 
+        host='0.0.0.0', 
+        port=int(os.environ.get('PORT', 5001))
+        )
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,127 @@
 from playwright.sync_api import Page, expect
+import pytest, os
 
 def test_get_index(db_connection, page, test_web_address):
     db_connection.seed("seeds/chitter.sql")
+    # Go to index page (not logged in)
     page.goto(f"http://{test_web_address}/")
-
+    # Compare all div tags with the classes expected of them
     div_list = page.locator("div")
     class_list = [
-        "container", "column", "column", "peep", "column"
+        "container", "column", "login-prompt", 
+        "column", "peep", "column"
     ]
     for div, class_label in zip(div_list.all(), class_list):
         expect(div).to_have_class(class_label)
-
+    # Check each peep-related div by class for expected contents
     peeps = page.locator(".peep")
     expect(peeps).to_have_id("1")
-
     timestamps = page.locator(".timestamp")
     expect(timestamps).to_have_text("2023-12-07 11:13:15")
-
     contents = page.locator(".content")
     expect(contents).to_have_text("Welcome to Chitter!")
-
     by_users = page.locator(".by-user")
     expect(by_users).to_have_text("JMcK4529")
+
+def test_get_login(db_connection, page, test_web_address):
+    """
+    Tests for:
+    Title = login to chitter
+    No error div class for standard get method
+    Three inputs: text, password, submit
+    """
+    db_connection.seed("seeds/chitter.sql")
+    # Go to login page and check its title
+    page.goto(f"http://{test_web_address}/login")
+    expect(page).to_have_title("Login to Chitter!")
+    # Check that there are no errors on initial load
+    div_list = page.locator("div").all()
+    assert all(".error" not in div.get_attribute("class") 
+               for div in div_list)
+    # Check that the expected input fields are present
+    input_list = page.locator("input").all()
+    expected_input_types = ["text", "password", "submit"]
+    for input, expected in zip(input_list, expected_input_types):
+        expect(input).to_have_attribute("type", expected)
+
+def test_index_checks_for_session(db_connection, page, test_web_address):
+    db_connection.seed("seeds/chitter.sql")
+    # Go to index page (not logged in)
+    page.goto(f"http://{test_web_address}/")
+    # Check if the login-prompt appears on page
+    div_list = page.locator("div").all()
+    assert any("login-prompt" in div.get_attribute("class") 
+               for div in div_list)
+    # Fill in the login form with a nonexistent user
+    page.get_by_label("Username").fill("NonUser")
+    page.get_by_label("Password").fill("FakePassword")
+    page.get_by_role("button").click()
+    # Check for redirect to login page
+    expect(page).to_have_url(f"http://{test_web_address}/login")
+    # Login with test/creator account, starting a session
+    page.get_by_label("Username").fill("JMcK4529")
+    page.get_by_label("Password").fill(os.getenv('CREATOR_PASS'))
+    page.get_by_role("button").click()
+    # Check for redirect to index page without login prompt
+    expect(page).to_have_url(f"http://{test_web_address}/")
+    div_list = page.locator("div").all()
+    assert all("login-prompt" not in div.get_attribute("class") 
+               for div in div_list)
+
+def test_post_login(db_connection, page, test_web_address):
+    """
+    Go to /login
+    Fill in form badly
+    Submit
+    Check for reload
+    Fill in form correctly
+    Submit
+    Check for redirect to /
+    """
+    db_connection.seed("seeds/chitter.sql")
+    # Go to login page
+    page.goto(f"http://{test_web_address}/login")
+    # Fill in form with non-existent user details
+    page.get_by_label("Username").fill("NonUser")
+    page.get_by_label("Password").fill("FakePassword")
+    page.get_by_role("button").click()
+    # Check there is no redirection
+    expect(page).to_have_url(f"http://{test_web_address}/login")
+    # Check there is an error div with the expected text
+    error_div = page.locator(".error")
+    expect(error_div).to_have_text("Username or password was incorrect.")
+    # Fill the form with test/creator details
+    page.get_by_label("Username").fill("JMcK4529")
+    page.get_by_label("Password").fill(os.getenv('CREATOR_PASS'))
+    page.get_by_role("button").click()
+    # Check for redirect to index page with username displayed
+    expect(page).to_have_url(f"http://{test_web_address}/")
+    assert "JMcK4529" in page.locator("#column1").inner_text()
+
+def test_logout(db_connection, page, test_web_address):
+    db_connection.seed("seeds/chitter.sql")
+    # Go to login page
+    page.goto(f"http://{test_web_address}/login")
+    # Sign in as test/creator
+    page.get_by_label("Username").fill("JMcK4529")
+    page.get_by_label("Password").fill(os.getenv('CREATOR_PASS'))
+    page.get_by_role("button").click()
+    # Check for redirect to index page with username displayed
+    expect(page).to_have_url(f"http://{test_web_address}/")
+    assert any("JMcK4529" in div.inner_text() 
+               for div in page.locator("div").all())
+    # Click Logout
+    page.get_by_role("button").click()
+    # Check for redirect to index page without user-details div
+    expect(page).to_have_url(f"http://{test_web_address}/")
+    print([div.get_attribute("class") for div in page.locator("div").all()])
+    assert all("user-details" not in div.get_attribute("class") 
+               for div in page.locator("div").all())
+
+@pytest.mark.skip(reason="Not yet implemented.")
+def test_get_signup(db_connection, page, test_web_address):
+    stuff=None
+
+@pytest.mark.skip(reason="Not yet implemented.")
+def test_post_signup(db_connection, page, test_web_address):
+    stuff=None
